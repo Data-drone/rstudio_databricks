6ceeb7809c62e62704b1f530751eea5b1f456829
Data-drone <bpl.law@gmail.com>
2021-08-05 23:46:12 +1000

    first working example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# rstudio_databricks
-Connecting RStudio to Databricks
+# Local RStudio with Databricks SQL Endpoint
+
+This is a guide on connecting RStudio on a local machine through to a Databricks SQL Endpoint as a service.
+
+
---
+++ docker_image/Example_Code/test_connection.R
@@ -0,0 +1,32 @@
+install.packages("odbc")
+
+library(DBI)
+library(tidyverse)
+library(dplyr)
+
+conn <- DBI::dbConnect(
+  odbc::odbc(),
+  dsn             = "Databricks-Spark",
+  Host            = Sys.getenv("DBSQL_HOST"),
+  Port            = 443,
+  SparkServerType = 3,
+  Schema          = "default",
+  ThriftTransport = 2,
+  SSL             = 1,
+  AuthMech        = 3,
+  UID             = "token",
+  PWD             = Sys.getenv("DBSQL_USERTOKEN"),
+  HTTPPath        = Sys.getenv("DBSQL_HTTPPATH")
+)
+
+# Use in_schema("schema", "database") for different database
+# note this example is for the nyctaxidelta table which is 
+# from the NYC taxi dataset which has been loaded and cleaned
+# See: 
+table <- tbl(conn, "nyctaxidelta") 
+
+average_fare_for_many_passenger <- table %>%
+  filter(passenger_count > 3) %>%
+  group_by(neighborhood) %>%
+  summarise(mean_fare = mean(fare_amount)) %>%
+  collect()
